6b84c682dc5b8cca0f8f77fa5afba5ed666a4715
Mitzi Zitler <mitzitler@gmail.com>
2025-03-31 19:12:28 -0400

    I deleted some stuff, commiting that, its all overwritten by better versions now

--- graveyard_code/mark.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    // okay its sooo awkward between bran, megan arnbul and megan keebler right now, right? did you notice anything?
-    // figures bran wouldn't bring it up. im sure hes nervous to bring that mess into work, today of all days
-
-    // ive been sketching up an org chart lately. its impossible. so many people have so many ambiguous titles
-    // do you want to see what i have going right now?
-</script>
