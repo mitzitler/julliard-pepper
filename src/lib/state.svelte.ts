30062947ebd119125e17c897e7889494d7541677
Mitzi Zitler <mitzitler@gmail.com>
2025-04-02 11:59:37 -0400

    Fixes to the interface options open and closed display

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,6 +1,46 @@
 import type { StandardDialogue, NonStandardDialogue } from "$lib/peopleDialogue.ts"
 import type { TaskListFull, TaskListSteps } from '$lib/tasks.ts'
 
+class InterfaceClass {
+    expandedId: string = $state('')
+    roomOptionsOpen: boolean = $state(false)
+
+
+    clickRoomScan() {
+        if (this.expandedId === '') {
+            this.expandedId = 'roomscan'
+            return
+        } else if (this.expandedId != '') {
+            this.expandedId = ''
+            return
+        }
+    } 
+
+    clickConversationOptions() {
+        if (this.expandedId === '') {
+            this.expandedId = 'dialogue'
+            return
+        } else if (this.expandedId != '') {
+            this.expandedId = ''
+            return
+        }
+    }
+
+    clickMoveRooms() {
+        if (this.expandedId === '') {
+            this.expandedId = 'moveroom'
+            this.roomOptionsOpen = true
+            return
+        } else if (this.expandedId != '') {
+            this.expandedId = ''
+            this.roomOptionsOpen = false
+            return
+        }
+    }
+}
+
+export const interfaceStuff = new InterfaceClass()
+
 class Location {
     code: string = $state("A")
     name: string = $state("Parking Lot")
@@ -19,8 +59,6 @@ class Location {
         this.moving = false
     }
 
-    openMoveRoom() {this.open = true}
-    closeMoveRoom() {this.open = false}
     newRoomInterface() {this.newRoom = true}
     oldRoomInterface() {this.newRoom = false}
 
@@ -50,6 +88,7 @@ class InventoryItems {
     usedItems: string[] = $state([]) 
     taskItemsToUse: string[] = $state([])
     isCurrentStepClosed: Record<string, boolean> = $state({})
+    open: boolean = $state(false)
 
     grabItem(item: string) {
         console.log("Adding item to inventory...")
@@ -63,6 +102,9 @@ class InventoryItems {
         this.usedItems.push(item)
     }
 
+    openItemScan() {this.open = true}
+    closeItemScan() {this.open = false}
+
     itemTaskStep(taskIdStep: string, tasksBySteps: TaskListSteps, location: string, discardItem?: string | null) {
         // this function is called on every room scan item monent
         // if it is needed to use the 
@@ -73,6 +115,7 @@ class InventoryItems {
         let subTaskType = taskStepInfo.task_type
         let subTaskTarget = taskStepInfo.target_name
         let subTaskTargetLocation = taskStepInfo.target_location
+        // let subTaskAnnouncement = taskStepInfo.step_completed_announcement
 
         if (subTaskType === 'grab_item') {
             if (this.inventoryDict.includes(subTaskTarget)) {
@@ -82,6 +125,7 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = true
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             } else {
                 console.log('User needs to grab the item', subTaskTarget, 'for the task', taskId)
@@ -91,6 +135,7 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = true
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
             }
             
         } else if (subTaskType === 'grab_discard_item') {
@@ -108,12 +153,14 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = false
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             } else if (!discardItem) {
                 console.log('This subtasktype requires you to have a discard item as well')
                 this.isCurrentStepClosed[taskIdStep] = false
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             } else if (this.inventoryDict.includes(discardItem)
                 && this.taskItemsToUse.includes(discardItem)
@@ -129,6 +176,7 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = true
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             // add more error statements
             } else {
@@ -136,6 +184,7 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = false
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             }
 
@@ -155,12 +204,14 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = false
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             } else if (!discardItem) {
                 console.log('This subtasktype requires you to have a discard item as well')
                 this.isCurrentStepClosed[taskIdStep] = false
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             } else if (this.inventoryDict.includes(discardItem)
                 && this.taskItemsToUse.includes(discardItem)
@@ -187,6 +238,7 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = true
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             // add more error statements
             } else {
@@ -194,6 +246,7 @@ class InventoryItems {
                 this.isCurrentStepClosed[taskIdStep] = false
 
                 return this.isCurrentStepClosed
+                // return this.isCurrentStepClosed, subTaskAnnouncement
 
             }
 
@@ -202,12 +255,14 @@ class InventoryItems {
             this.isCurrentStepClosed[taskIdStep] = false
 
             return this.isCurrentStepClosed
+            // return this.isCurrentStepClosed, subTaskAnnouncement
 
         } else {
             console.log('something went wrong with the function itemTaskStep, probably no sub task type found')
             this.isCurrentStepClosed[taskIdStep] = false
 
             return this.isCurrentStepClosed
+            // return this.isCurrentStepClosed, subTaskAnnouncement
 
         }
     }
@@ -264,27 +319,31 @@ class Tasks {
         console.log(this.stepsTasks)
     }
 
+    // renamed this??
     incrementTask(taskId: string, tasksBySteps: TaskListSteps) {
         let taskSteps: string[] = Object.keys(tasksBySteps[taskId].steps)
         let seenSteps: string[] = Object.keys(this.stepsTasks[taskId])
         let stepCount: number = taskSteps.length
         let seenStepsCount: number = seenSteps.length
-        // let mostRecentStep: string = seenSteps[seenStepsCount-1]
+        let mostRecentStep: string = seenSteps[seenStepsCount-1]
         let done: boolean = false
         let nextTaskStep: null | string = null
-        let nextTaskStepTarget: null | string = null
+        // let nextTaskStepTarget: null | string = null
+        let stepAnnouncement: null | string = null
 
         if (this.completedTasks.includes(taskId) || this.failedTasks.includes(taskId)) {
             console.log('This task is seen through to the end.')
             done = true
-            return done
+            return [done, null]
 
         } else if (seenStepsCount === stepCount && done === false) {
             console.log('For the task', taskId, 'you are on the last step:', taskSteps[stepCount-1])
             console.log('There is nothing left to increment and you have finished the task')
-            // return [nextTaskStep, nextTaskStepTarget]
+            // return done, nextTaskStepTarget
             done = true
-            return done
+            stepAnnouncement = tasksBySteps[taskId]?.steps[mostRecentStep].step_completed_announcement
+    
+            return [done, stepAnnouncement]
 
         } else if (seenStepsCount < stepCount && done === false) {
             console.log('There are more steps left to increment for the task', taskId)
@@ -293,17 +352,15 @@ class Tasks {
             nextTaskStep = taskSteps[seenStepsCount]
             this.stepsTasks[taskId].push(nextTaskStep)
             console.log('The subtasks now seen for this task are', this.stepsTasks[taskId].join(','))
-            nextTaskStepTarget = tasksBySteps[taskId].steps[nextTaskStep].target_type
-            // return [nextTaskStep, nextTaskStepTarget]
+            stepAnnouncement = tasksBySteps[taskId]?.steps[mostRecentStep].step_completed_announcement
             done = false
-            return done
+            return [done, stepAnnouncement]
 
         } else {
             console.log('something went wrong with the function incrementTask')
-            // return [nextTaskStep, nextTaskStepTarget]
+            // return done, nextTaskStepTarget
             done = false
-            return done
-
+            return [done, null]
         }
     }
 
@@ -342,6 +399,11 @@ class Conversations {
     consumesTime: boolean = $state(false)
     taskAvailable: boolean = $state(false)
     taskId: string = $state('')
+    isCurrentStepClosed: Record<string, boolean> = $state({})
+    open: boolean = $state(false)
+
+    openConversationOptions() {this.open = true}
+    closeConversationOptions() {this.open = false}
 
     startConversation(guy: string) {
         this.character = guy
@@ -363,10 +425,16 @@ class Conversations {
         }
     }
 
+    // lets do this 
+    // get dialogue type, which says either nonstandard or standard
+    // and then in the component its an if / if else 
+    // this way i can put more complicated task grabbing stuff in a dedicated function
+
     getDialogue(
         guy: string, 
         locationCode: string, 
-        activeTasks: string[],
+        // should this be steps tasks, or just tasks?
+        getMostActiveStepsTasks: string[],
         seenTasks: string[], 
         standardDialogueDict: StandardDialogue, 
         nonStandardDialogueDict: NonStandardDialogue,
@@ -379,6 +447,10 @@ class Conversations {
         let initiateTask = ''
         let dialogueType = ''
 
+        // actually we are already doing this
+        // let dialogueReturn: string[] | null[] = []
+        // return [dialogue, taskIdStep, dialogueType]
+
         responseDialogue: { 
             if (!nonStandardDialogueDict[guy]) {
                 console.log(guy, "actually does not have any non-standard dialogue")
@@ -389,14 +461,15 @@ class Conversations {
                 break responseDialogue
             } else if (Object.keys(nonStandardDialogueDict[guy]).includes('X')) {
                 characterHasResponses = true
-                for (const taskId of Object.keys(nonStandardDialogueDict[guy]['X'])) {
-                    if (activeTasks.includes(taskId) && succeed === true) { // for now its always succeed
-                        console.log('Task ', taskId, ' is open and can be resolved by ', guy, '...')
-                        this.dialogueArray = nonStandardDialogueDict[guy]['X'][taskId]['succeed']
+                for (const taskIdStep of Object.keys(nonStandardDialogueDict[guy]['X'])) {
+                    let taskId = `${taskIdStep.split('-')[0]}-${taskIdStep.split('-')[1]}`
+                    if (getMostActiveStepsTasks.includes(taskIdStep) && succeed === true) { // for now its always succeed
+                        console.log('Task step', taskIdStep, ' is open and can be resolved by ', guy, '...')
+                        this.dialogueArray = nonStandardDialogueDict[guy]['X'][taskIdStep]['succeed']
                         this.consumesTime = true
                         this.conversationHistory.push(this.dialogueArray)
                         dialogueType = 'taskResponse'
-                        return [this.dialogueArray, taskId, dialogueType]
+                        return [this.dialogueArray, taskIdStep, dialogueType]
                     } else {
                         console.log('There are no active tasks that ', guy, ' could be responding to')
                         break responseDialogue
@@ -472,8 +545,45 @@ class Conversations {
 
     }
 
-    incrementTaskDialogue(guy: string) {
-        
+    conversationTaskStep(taskIdStep: string, tasksBySteps: TaskListSteps, guy: string) {
+
+        // this will be either go speak or speech check
+        // for now, just go speak
+        // this is called on the final dialogue button
+
+        let taskId = `${taskIdStep.split('-')[0]}-${taskIdStep.split('-')[1]}`
+        let taskStepInfo = tasksBySteps[taskId].steps[taskIdStep]
+        let subTaskType = taskStepInfo.task_type
+        let subTaskTarget = taskStepInfo.target_name // .toLower()
+        let subTaskAnnouncement: string | null = taskStepInfo.step_completed_announcement
+
+        if (subTaskType === 'go_speak') {
+            console.log('For the conversation with', guy, "you are completing task step", taskIdStep,
+                "with a go_speak type interaction. this has the announcement", subTaskAnnouncement)
+
+            this.isCurrentStepClosed[taskIdStep] = true
+            return this.isCurrentStepClosed, subTaskAnnouncement
+
+        } else if (subTaskType === 'speech_check') {
+            console.log('For the conversation with', guy, "you are completing task step", taskIdStep,
+                "with a go_speak type interaction. this has the announcement", subTaskAnnouncement)
+            console.log('This is a speech check, which is not set up yet')
+
+            this.isCurrentStepClosed[taskIdStep] = false
+            subTaskAnnouncement = null
+            return this.isCurrentStepClosed, subTaskAnnouncement
+
+        } else if (subTaskType === 'grab_item' || subTaskType === 'grab_discard_item' || subTaskType === 'use_discard_item') {
+
+            console.log('subTaskType =', subTaskTarget, "this step is intended for items")
+            this.isCurrentStepClosed[taskIdStep] = false
+            subTaskAnnouncement = null
+            return this.isCurrentStepClosed, subTaskAnnouncement
+
+        } else {
+            console.log("there was some error and the sub task type was not found")
+        }
+
     }
 
     getTaskResponseDialogue(guy: string, activeTasks: string[], nonStandardDialogueDict: NonStandardDialogue, succeed: boolean) {
