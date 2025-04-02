884af845491a674d228fdd137f02e9a17da74ed9
Mitzi Zitler <mitzitler@gmail.com>
2025-04-02 12:00:09 -0400

    More work on dialogue side of task completion

--- src/components/display/Dialogue.svelte
+++ src/components/display/Dialogue.svelte
@@ -1,14 +1,16 @@
-<script>
+<script lang="ts">
 // @ts-nocheck
-
+    import { createEventDispatcher } from 'svelte';
     // class for tasks too
     import { location, localTime, conversation, tasks } from '$lib/state.svelte.js'
     import { dialogueAllStandard, dialogueAllNonStandard } from '$lib/peopleDialogue.ts'
     import { tasksBySteps } from '$lib/tasks.ts';
 
+    const dispatch = createEventDispatcher();
+
     let seenTasks = tasks.activeTasks + tasks.completedTasks + tasks.failedTasks
     let dialoguePosition = 0
-    let [dialogueArray, taskId, dialogueType] = conversation.getDialogue(
+    let [dialogueArray, taskIdOrStep, dialogueType] = conversation.getDialogue(
         conversation.character, 
         location.code, 
         tasks.activeTasks, 
@@ -17,8 +19,75 @@
         dialogueAllNonStandard
     )
 
-    console.log('tasklist ??', Object.keys(tasksBySteps['clare-2']['steps']))
+    const handleEndConversationClick = () => {
 
+        // task time
+        console.log('conversation is ended and time has added - time to complete task requirements')
+        console.log('is this dialogue a task initiation, or a step?')
+        let taskIdStep: string | null = null
+        let taskId: string | null = null
+        // if the third part of splitting by - is length 1, as opposed to null
+        if (taskIdOrStep.split('-')[2]?.length === 1) {
+            taskIdStep = taskIdOrStep
+            taskId = `${taskIdStep.split('-')[0]}-${taskIdStep.split('-')[1]}`
+
+        } else {
+            taskIdStep = null
+            taskId = taskIdOrStep
+        }
+
+        // the item version completes the task because of the return on the incrementTask function
+        // for now, this version completes the task if total steps === seen steps
+        let stepsCount = Object.keys(tasksBySteps[taskId].steps).length
+        let stepsSeenCount = Object.keys(tasks.stepsTasks[taskId]).length
+        let taskDone: string | null = null 
+        let stepAnnouncement: string | null = null
+
+        if (dialogueType === 'taskResponse' && tasks.activeTasks?.includes(taskId)) {
+            let increment = incrementTask(taskId, tasksBySteps)
+
+            if (increment === true) {
+                tasks.completeTask(taskId)
+                conversation.endConversation(conversation.character)
+            } else if (increment === false) {
+                conversation.conversationTaskStep(
+                taskIdStep,
+                tasksBySteps,
+                conversation.character
+                )
+                [taskDone, stepAnnouncement] = tasks.incrementTask(taskId, tasksBySteps)
+                conversation.endConversation(conversation.character);
+            }
+
+            localTime.addTime()
+        } else if (dialogueType === 'taskResponse' && !tasks.activeTasks?.includes(taskId)) {
+            // task is not active
+            console.log('Your dialogue is supposed to respond to a task, but that task has not be initiated or is already completed')
+        } else if (dialogueType === 'taskInitiate') {
+            // this starts a task
+            tasks.addTask(taskId)
+            tasks.startTask(taskId, tasksBySteps)
+            [taskDone, stepAnnouncement] = tasks.incrementTask(taskId, tasksBySteps)
+            conversation.endConversation(conversation.character);
+            localTime.addTime()
+        } else if (dialogueType === 'main' | dialogueType === 'secondary' | dialogueType === 'default') {
+            // this is just time consuming dialogue
+            console.log('nothing task related happened')
+            conversation.endConversation(conversation.character);
+
+            (dialogueType === 'main' && localTime.addTime())
+        } else {
+            // total error
+            console.log('Error: probably, dialogueType is null')
+        }
+
+        conversation.closeConversationOptions()
+        
+        function handleStepCompleteConversation() {
+            dispatch('conversation-ended', {stepAnnouncement: stepAnnouncement});
+        }
+
+    };
 
 </script>
 
@@ -41,18 +110,7 @@
             </li>
             <button key={index} 
                 class={conversation.consumesTime ? 'indicates-consuming-time' : ''}
-                onclick={() => 
-                {conversation.endConversation(conversation.character);
-                {conversation.consumesTime && localTime.addTime()};
-                {console.log("This is the end of the conversation, time to do task things")}
-                { 
-                    dialogueType === 'taskResponse' ? 
-                    tasks.activeTasks.includes(taskId) && tasks.completeTask(taskId) :
-                        (dialogueType === 'taskInitiate' ? (
-                            tasks.addTask(taskId),
-                            tasks.startTask(taskId, tasksBySteps)) : 
-                        console.log('Nothing task-related happened') )}
-                }}> 
+                onclick={() => handleEndConversationClick}> 
                 End conversation
             </button>
 
