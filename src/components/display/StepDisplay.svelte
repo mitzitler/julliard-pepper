c1b5bdacf6bd4a69853e769ec81a309948b19f13
Mitzi Zitler <mitzitler@gmail.com>
2025-04-07 13:51:25 -0400

    retrieved info

---
+++ src/components/display/StepDisplay.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+    export function scaleStepAnnouncement(stepAnnouncement: string | null) {
+        console.log('this is the announcement:', stepAnnouncement)
+    }
+    
+
+</script>
+
+<div class="step">
+    <!-- import children ? -->
+    <!-- {children} -->
+     hi
+</div>
+
+<style>
+    .step {
+        background-color: aquamarine;
+    }
+</style>
